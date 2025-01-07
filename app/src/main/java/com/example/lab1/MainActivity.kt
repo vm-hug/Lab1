95892c0c61a335ac4d19b3b6c6a1f88493f5a867
vm-hug <minhhung191205@gmail.com>
2025-01-07 09:52:08 +0700

    test lab1

---
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.lab1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.lab1.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        xuly()
+    }
+
+    private fun xuly() {
+        binding.btnRamdom.setOnClickListener{
+            var soRamdom = Random.nextInt(0, 100)
+            binding.txtRamdom.setText(soRamdom.toString())
+        }
+    }
+
+}
